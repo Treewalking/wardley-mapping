60b8e357e727c6b9286c1864a7c99d97d790a992
batemanp <psbateman@gmail.com>
2022-06-12 15:13:29 +0100

    Beginnings of Model: Added JUnit Publisher

--- src/main/java/org/treewalking/wardley/model/Component.java
+++ src/main/java/org/treewalking/wardley/model/Component.java
@@ -1,8 +1,10 @@
 package org.treewalking.wardley.model;
 
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @EqualsAndHashCode
+@ToString
 public class Component {
     private final Type type;
     private final String name;
